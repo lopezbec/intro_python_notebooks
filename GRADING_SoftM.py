f9d8f47bb91cb2f3eb629ee6dd708cf3bf0756df
Christian Lopez <54818552+lopezbec@users.noreply.github.com>
2023-02-09 11:18:27 -0500

    Create GRADING_SoftM.py

---
+++ GRADING_SoftM.py
@@ -0,0 +1,32 @@
+def GRADING_SoftM(accuracy_score_1, accuracy_score_l2,accuracy_score_estop,accuracy_score_final):
+
+
+    grades={"accuracy_score_1":False, 
+            "accuracy_score_l2":False, 
+            "accuracy_score_estop":False,
+            "accuracy_score_final":False}
+
+
+    try:
+        if(accuracy_score_1==0.9): grades["accuracy_score_1"]=True
+    except: grades["accuracy_score_1"]=False
+
+    try:
+        if(accuracy_score_l2==1.0 ): grades["accuracy_score_l2"]=True
+    except: grades["accuracy_score_l2"]=False
+
+
+    try:
+        if(accuracy_score_estop==1.0): grades["accuracy_score_estop"]=True
+    except: grades["accuracy_score_estop"]=False
+    
+    try:
+        if(accuracy_score_final==0.9333333333333333): grades["accuracy_score_final"]=True
+    except: grades["accuracy_score_final"]=False
+
+
+
+    for x in grades:
+        print (x,':',grades[x])
+        
+    
