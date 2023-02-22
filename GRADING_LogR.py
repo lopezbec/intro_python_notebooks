19aeb57ef9a8e4b6567aadf9b1ba13db6a891585
Christian Lopez <54818552+lopezbec@users.noreply.github.com>
2023-02-22 12:43:54 -0500

    Update GRADING_LogR.py

--- GRADING_LogR.py
+++ GRADING_LogR.py
@@ -12,30 +12,30 @@ def GRADING_LogR(sigmoid,cost,grad,theta_GD,cost_GD,prob_new_student,cost_Reg,gr
         if( sigmoid(0)==0.5): grades["sigmoid"]=True
     except: grades["sigmoid"]=False
     try:
-        if(cost==0.2183301938265977): grades["cost"]=True
+        if(round(cost,3)==round(0.2183301938265977,3)): grades["cost"]=True
     except: grades["cost"]=False
     try:
-        if(grad[1]==2.5662341155107558): grades["grad"]=True
+        if(round(grad[1],3)==round(2.5662341155107558,3)): grades["grad"]=True
     except: grades["grad"]=False
 
     try:
-        if(theta_GD[2]==3.7260375718483822): grades["theta_GD"]=True
+        if(round(theta_GD[2],3)==round(3.7260375718483822,3)): grades["theta_GD"]=True
     except: grades["theta_GD"]=False
 
     try:
-        if(cost_GD==0.2034977556513879): grades["cost_GD"]=True
+        if(round(cost_GD,3)==round(0.2034977556513879,3)): grades["cost_GD"]=True
     except: grades["cost_GD"]=False
 
     try:
-        if(prob_new_student[0,0]==0.7762466784809318): grades["predict"]=True
+        if(round(prob_new_student[0,0],3)==round(0.7762466784809318,3): grades["predict"]=True
     except: grades["predict"]=False
 
     try:
-        if(cost_Reg[0]==2.134848314665857): grades["cost_Reg"]=True
+        if(round(cost_Reg[0],3)==round(2.134848314665857,3)): grades["cost_Reg"]=True
     except: grades["cost_Reg"]=False
 
     try:
-        if(grad_Reg[4]==0.015914488662613836): grades["grad_Reg"]=True
+        if(round(grad_Reg[4],3)==round(0.015914488662613836,3)): grades["grad_Reg"]=True
     except: grades["grad_Reg"]=False
 
 
